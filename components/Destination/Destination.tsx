ca6e75758501c4ce3197855fee08cac8ab3f6614
webala <webd01@protonmail.com>
2022-12-08 08:53:11 +0300

    feat: destinations functionality.

--- components/Destination/Destination.tsx
+++ components/Destination/Destination.tsx
@@ -2,22 +2,66 @@ import Image from "next/image";
 import React from "react";
 import style from "./Destination.module.scss";
 
-function Destination({destination, img}) {
+function Destination({ destination, img, setDestination, destData }) {
 	return (
 		<div className={style.destination}>
 			<div className={style.hero}>
 				<div className={style.title}>
-					<h1><span>01</span>  pick your destination</h1>
+					<h1>
+						<span>01</span> pick your destination
+					</h1>
 				</div>
-				<Image className={style.destination_image} src={img} width={300} height={300} alt="destination" />
+				<Image
+					className={style.destination_image}
+					src={img}
+					width={300}
+					height={300}
+					alt="destination"
+				/>
 			</div>
 
 			<div className={style.details}>
 				<div className={style.nav}>
-					<p>MOON</p>
-					<p>MARS</p>
-					<p>EUROPA</p>
-					<p>TITAN</p>
+					<p
+						className={
+							destination.name === "Moon"
+								? `${style.link} ${style.focus}`
+								: style.link
+						}
+						onClick={() => setDestination(destData[0])}
+					>
+						MOON
+					</p>
+					<p
+						className={
+							destination.name === "Mars"
+								? `${style.link} ${style.focus}`
+								: style.link
+						}
+						onClick={() => setDestination(destData[1])}
+					>
+						MARS
+					</p>
+					<p
+						className={
+							destination.name === "Europa"
+								? `${style.link} ${style.focus}`
+								: style.link
+						}
+						onClick={() => setDestination(destData[2])}
+					>
+						EUROPA
+					</p>
+					<p
+						className={
+							destination.name === "Titan"
+								? `${style.link} ${style.focus}`
+								: style.link
+						}
+						onClick={() => setDestination(destData[3])}
+					>
+						TITAN
+					</p>
 				</div>
 				<div className={style.title}>
 					<h1>{destination.name}</h1>
--- pages/destinations.tsx
+++ pages/destinations.tsx
@@ -1,20 +1,36 @@
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navigation from "../components/Navigation/Navigation";
 import data from "../data.json";
 import styles from "../styles/global.module.scss";
 import moonImage from "../public/assets/destination/image-moon.webp";
+import marsImage from  "../public/assets/destination/image-mars.webp";
+import europaImage from  "../public/assets/destination/image-europa.webp";
+import titanImage from  "../public/assets/destination/image-titan.webp";
 import Destination from "../components/Destination/Destination";
 
 function destinations() {
 	const destData = data.destinations;
 	const [destination, setDestination] = useState(destData[0]);
+    const [img, setImg] = useState(moonImage)
 	console.log(destData);
+
+    useEffect(() => {
+        if (destination.name === 'Mars'){
+            setImg(marsImage)
+        } else if (destination.name === "Moon"){
+            setImg(moonImage)
+        } else if (destination.name === "Titan"){
+            setImg(titanImage)
+        } else if (destination.name === "Europa"){
+            setImg(europaImage)
+        }
+    }, [destination])
 
 	return (
 		<div className={styles.container}>
 			<Navigation />
-			<Destination destination={destination} img={moonImage}/>
+			<Destination destination={destination} img={img} setDestination={setDestination} destData={destData}/>
 		</div>
 	);
 }
